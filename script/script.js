8f3e1a0b6c2323ed382b3d95a15a91577879b469
Mohamed-Farraj <mohamedfarraj27@gmail.com>
2024-07-10 12:37:58 +0300

    implement first 3 section

---
+++ script/script.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#typed', {
+    strings: ['Freelancer', 'Developer','Photographer','Designer'],
+    typeSpeed: 50,
+    startDelay:500,
+    backDelay:1000,
+    loop:true,
+    fadeOut:true,
+    backSpeed:35,
+  });
